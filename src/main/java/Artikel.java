faa8a7c2c4652f9506315f35b559d86fb31e5ee7
Milan Schuringa <milanschuringa@icloud.com>
2020-05-19 16:23:24 +0200

    Initial commit

---
+++ src/main/java/Artikel.java
@@ -0,0 +1,29 @@
+public class Artikel {
+
+    private String naam;
+    private double prijs;
+
+    public Artikel(String naam, double prijs) {
+        this.naam = naam;
+        this.prijs = prijs;
+    }
+
+    public Artikel() {
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public double getPrijs() {
+        return prijs;
+    }
+
+    public void setPrijs(double prijs) {
+        this.prijs = prijs;
+    }
+}
---
+++ src/main/java/Persoon.java
@@ -0,0 +1,61 @@
+public class Persoon {
+
+    private int BSN;
+    private String voorNaam;
+    private String achterNaam;
+    private int geboorteDatum;
+    private char geslacht;
+
+    public Persoon(int BSN, String voorNaam, String achterNaam, int geboorteDatum, char geslacht) {
+        this.BSN = BSN;
+        this.voorNaam = voorNaam;
+        this.achterNaam = achterNaam;
+        this.geboorteDatum = geboorteDatum;
+        this.geslacht = geslacht;
+    }
+
+    public Persoon() {
+    }
+
+    public int getBSN() {
+        return BSN;
+    }
+
+    public void setBSN(int BSN) {
+        this.BSN = BSN;
+    }
+
+    public String getVoorNaam() {
+        return voorNaam;
+    }
+
+    public void setVoorNaam(String voorNaam) {
+        this.voorNaam = voorNaam;
+    }
+
+    public String getAchterNaam() {
+        return achterNaam;
+    }
+
+    public void setAchterNaam(String achterNaam) {
+        this.achterNaam = achterNaam;
+    }
+
+    public int getGeboorteDatum() {
+        return geboorteDatum;
+    }
+
+    public void setGeboorteDatum(int geboorteDatum) {
+        this.geboorteDatum = geboorteDatum;
+    }
+
+    public char getGeslacht() {
+        return geslacht;
+    }
+
+    public void setGeslacht(char geslacht) {
+        if (geslacht = "M" || geslacht = "V") {
+            this.geslacht = geslacht;
+        }
+    }
+}
