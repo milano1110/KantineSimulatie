c6987a4b2eb805f3223e5897d2f7b37748893074
Milan Schuringa <milanschuringa@gmail.com>
2020-05-19 16:32:17 +0200

    modified

--- src/main/java/Datum.java
+++ src/main/java/Datum.java
@@ -6,12 +6,60 @@ public class Datum {
 
 	/**
 	 * Constructor
+	 * @param dag
+	 * @param maand
+	 * @param jaar
 	 */
-	// TODO
+	public Datum(int dag, int maand, int jaar) {
+		this.dag = dag;
+		this.maand = maand;
+		this.jaar = jaar;
+	}
+
+	public Datum() {
+		this.dag = 0;
+		this.maand = 0;
+		this.jaar = 0;
+	}
+
+	public int getDag() {
+		return dag;
+	}
+
+	public void setDag(int dag) {
+		this.dag = dag;
+	}
+
+	public int getMaand() {
+		return maand;
+	}
+
+	public void setMaand(int maand) {
+		this.maand = maand;
+	}
+
+	public int getJaar() {
+		return jaar;
+	}
+
+	public void setJaar(int jaar) {
+		this.jaar = jaar;
+	}
 
 	public boolean bestaatDatum(int dag, int maand, int jaar) {
-		// TODO
-		return false;
+		if (dag < 1) {
+			return false;
+		}
+
+		if (maand < 1 || maand > 12) {
+			return false;
+		}
+
+		if (jaar < 1900 || jaar > 2100) {
+			return false;
+		}
+		return true;
+
 	}
 
 	/**
